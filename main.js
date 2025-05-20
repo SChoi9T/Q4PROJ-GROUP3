48af92265d3536ceb278dca5b95e95bdb72e754f
harhar <yao.hgk@obmontessori.edu.ph>
2025-05-20 23:50:49 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,161 @@
+// ABELEDA JS
+
+function rating$leona(){
+	var score = document.getElementById('rating-leona').value;
+
+	switch (score){
+	case "1":
+		document.getElementById('rate-score-leona').innerHTML ="1/5";
+		break;
+
+	case "2":
+		document.getElementById('rate-score-leona').innerHTML ="2/5";
+		break;
+
+	case "3":
+		document.getElementById('rate-score-leona').innerHTML ="3/5";
+		break;
+
+	case "4":
+		document.getElementById('rate-score-leona').innerHTML ="4/5";
+		break;
+
+	case "5":
+		document.getElementById('rate-score-leona').innerHTML ="5/5";
+		break;
+
+	default:
+		document.getElementById('rate-score-leona').innerHTML ="0/5";
+
+	}
+}
+
+// CHOI JS
+
+function rating$choi(){
+	var score = document.getElementById('rating-choi').value;
+
+	switch (score){
+	case "1":
+		document.getElementById('rate-score-choi').innerHTML ="1/5";
+		break;
+
+	case "2":
+		document.getElementById('rate-score-choi').innerHTML ="2/5";
+		break;
+
+	case "3":
+		document.getElementById('rate-score-choi').innerHTML ="3/5";
+		break;
+
+	case "4":
+		document.getElementById('rate-score-choi').innerHTML ="4/5";
+		break;
+
+	case "5":
+		document.getElementById('rate-score-choi').innerHTML ="5/5";
+		break;
+
+	default:
+		document.getElementById('rate-score-choi').innerHTML ="0/5";
+
+	}
+}
+
+
+// COTIOCO JS
+
+function rating$sean(){
+	var score = document.getElementById('rating-sean').value;
+
+	switch (score){
+	case "1":
+		document.getElementById('rate-score-sean').innerHTML ="1/5";
+		break;
+
+	case "2":
+		document.getElementById('rate-score-sean').innerHTML ="2/5";
+		break;
+
+	case "3":
+		document.getElementById('rate-score-sean').innerHTML ="3/5";
+		break;
+
+	case "4":
+		document.getElementById('rate-score-sean').innerHTML ="4/5";
+		break;
+
+	case "5":
+		document.getElementById('rate-score-sean').innerHTML ="5/5";
+		break;
+
+	default:
+		document.getElementById('rate-score-sean').innerHTML ="0/5";
+
+	}
+}
+
+
+// ONG JS
+
+function rating$ong(){
+	var score = document.getElementById('rating-ong').value;
+
+	switch (score){
+	case "1":
+		document.getElementById('rate-score-ong').innerHTML ="1/5";
+		break;
+
+	case "2":
+		document.getElementById('rate-score-ong').innerHTML ="2/5";
+		break;
+
+	case "3":
+		document.getElementById('rate-score-ong').innerHTML ="3/5";
+		break;
+
+	case "4":
+		document.getElementById('rate-score-ong').innerHTML ="4/5";
+		break;
+
+	case "5":
+		document.getElementById('rate-score-ong').innerHTML ="5/5";
+		break;
+
+	default:
+		document.getElementById('rate-score-ong').innerHTML ="0/5";
+
+	}
+}
+
+// YAO JS
+
+function rating$yao(){
+	var score = document.getElementById('rating-yao').value;
+
+	switch (score){
+	case "1":
+		document.getElementById('rate-score-yao').innerHTML ="1/5";
+		break;
+
+	case "2":
+		document.getElementById('rate-score-yao').innerHTML ="2/5";
+		break;
+
+	case "3":
+		document.getElementById('rate-score-yao').innerHTML ="3/5";
+		break;
+
+	case "4":
+		document.getElementById('rate-score-yao').innerHTML ="4/5";
+		break;
+
+	case "5":
+		document.getElementById('rate-score-yao').innerHTML ="5/5";
+		break;
+
+	default:
+		document.getElementById('rate-score-yao').innerHTML ="0/5";
+
+	}
+}
